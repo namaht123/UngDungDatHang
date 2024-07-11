eb10ea234a55e7ed9be7c55a206dc5c38545222d
Nam <sam@google.com>
2024-07-11 12:44:41 +0700

    nam

--- app/src/main/java/com/example/ung_dung_dat_hang/View/ManHinhChao/ManHinhChaoActivity.java
+++ app/src/main/java/com/example/ung_dung_dat_hang/View/ManHinhChao/ManHinhChaoActivity.java
@@ -1,4 +1,4 @@
-package com.example.ung_dung_dat_hang;
+package com.example.ung_dung_dat_hang.View.ManHinhChao;
 
 import android.os.Bundle;
 
@@ -8,13 +8,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.ung_dung_dat_hang.R;
 
+public class ManHinhChaoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.manhinhchao_layout);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
