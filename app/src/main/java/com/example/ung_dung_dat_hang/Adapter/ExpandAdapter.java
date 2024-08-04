69b8630bee1c3fa41aea776a21853bcfad4289b3
Nam <sam@google.com>
2024-08-04 22:58:49 +0700

    nam updatemenu

--- app/src/main/java/com/example/ung_dung_dat_hang/Adapter/ExpandAdapter.java
+++ app/src/main/java/com/example/ung_dung_dat_hang/Adapter/ExpandAdapter.java
@@ -6,11 +6,13 @@ import android.graphics.Point;
 import android.util.Log;
 import android.view.Display;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.ExpandableListView;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -25,6 +27,7 @@ public class ExpandAdapter extends BaseExpandableListAdapter {
 
     Context context;
     List<LoaiSanPham> loaiSanPhams;
+    ViewHolderMenu viewHolderMenu;
 
     public ExpandAdapter(Context context, List<LoaiSanPham> loaiSanPhams){
         this.context = context;
@@ -62,7 +65,12 @@ public class ExpandAdapter extends BaseExpandableListAdapter {
 
     @Override
     public int getChildrenCount(int vitriGroupCha) {
-        return 1;
+        if (loaiSanPhams.get(vitriGroupCha).getListCon().size() != 0){
+            return 1;
+        }else {
+            return 0;
+        }
+
     }
 
     @Override
@@ -90,25 +98,70 @@ public class ExpandAdapter extends BaseExpandableListAdapter {
         return false;
     }
 
+    public class ViewHolderMenu{
+        TextView txtTenLoaiSP;
+        ImageView hinhMenu;
+
+    }
+
     @Override
     public View getGroupView(int vitriGroupCha, boolean isExpanded, View view, ViewGroup viewGroup) {
-        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View viewGroupCha = layoutInflater.inflate(R.layout.custum_lauout_group_cha, viewGroup, false);
-        TextView txtTenLoaiSP = (TextView) viewGroupCha.findViewById(R.id.txtTenLoaiSP);
-        txtTenLoaiSP.setText(loaiSanPhams.get(vitriGroupCha).getTENLOAISP());
+
+        View viewGroupCha = view;
+
+        if (viewGroupCha == null) {
+            viewHolderMenu = new ViewHolderMenu();
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            viewGroupCha = layoutInflater.inflate(R.layout.custum_lauout_group_cha, viewGroup, false);
+
+            viewHolderMenu.hinhMenu = (ImageView) viewGroupCha.findViewById(R.id.imMenuPlus);
+            viewHolderMenu.txtTenLoaiSP = (TextView) viewGroupCha.findViewById(R.id.txtTenLoaiSP);
+
+            viewGroupCha.setTag(viewHolderMenu);
+        } else {
+            viewHolderMenu = (ViewHolderMenu) viewGroupCha.getTag();
+        }
+
+
+        viewHolderMenu.txtTenLoaiSP.setText(loaiSanPhams.get(vitriGroupCha).getTENLOAISP());
+
+        int demSanPhamCon = loaiSanPhams.get(vitriGroupCha).getListCon().size();
+        if (demSanPhamCon > 0) {
+            viewHolderMenu.hinhMenu.setVisibility(View.VISIBLE);
+        } else {
+            viewHolderMenu.hinhMenu.setVisibility(View.INVISIBLE);
+        }
+
+        if (isExpanded) {
+            viewHolderMenu.hinhMenu.setImageResource(R.drawable.icon_block);
+            viewGroupCha.setBackgroundResource(R.color.cologray);
+        } else {
+            viewHolderMenu.hinhMenu.setImageResource(R.drawable.icon_add);
+        }
+
+        viewGroupCha.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.d("loaisp", loaiSanPhams.get(vitriGroupCha).getTENLOAISP() + " - " + loaiSanPhams.get(vitriGroupCha).getMALOAISP());
+                return false;
+            }
+        });
+
 
         return viewGroupCha;
     }
 
     @Override
     public View getChildView(int vitriGroupCha, int vitriGroupCon, boolean isLastChild, View view, ViewGroup viewGroup) {
-        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 //        View viewGroupCon = layoutInflater.inflate(R.layout.custom_layout_group_con, viewGroup, false);
 //
 //        ExpandableListView expandableListView = (ExpandableListView) viewGroupCon.findViewById(R.id.epMenuCon);
         SecondExpanable secondExpanable = new SecondExpanable(context);
-        SecondAdapter secondAdapter = new SecondAdapter(loaiSanPhams.get(vitriGroupCha).getListCon());
+        ExpandAdapter secondAdapter = new ExpandAdapter(context,loaiSanPhams.get(vitriGroupCha).getListCon());
         secondExpanable.setAdapter(secondAdapter);
+
+        secondExpanable.setGroupIndicator(null);
         notifyDataSetChanged();
 
         return secondExpanable;
@@ -130,7 +183,7 @@ public class ExpandAdapter extends BaseExpandableListAdapter {
             int heiht = size.y;
             Log.d("size",width + " - " + heiht);
 
-            widthMeasureSpec = MeasureSpec.makeMeasureSpec(900,MeasureSpec.AT_MOST);
+//            widthMeasureSpec = MeasureSpec.makeMeasureSpec(900,MeasureSpec.AT_MOST);
             heightMeasureSpec = MeasureSpec.makeMeasureSpec(1080, MeasureSpec.AT_MOST);
             super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         }
@@ -142,92 +195,92 @@ public class ExpandAdapter extends BaseExpandableListAdapter {
     }
 
 
-    public class SecondAdapter extends BaseExpandableListAdapter{
-        List<LoaiSanPham> listCon;
-        public SecondAdapter(List<LoaiSanPham> listCon){
-            this.listCon = listCon;
-            XuLyJSONMenu xuLyJSONMenu = new XuLyJSONMenu();
-
-            for (int i = 0; i < listCon.size(); i++) {
-                int maloaisp = listCon.get(i).getMALOAISP();
-                final int index = i; // Make the variable effectively final
-                xuLyJSONMenu.LayLoaiSanPhamThaoMaLoai(maloaisp, new DownloadJSONCallback() {
-                    @Override
-                    public void onSuccess(String data) {
-                        List<LoaiSanPham> subCategories = xuLyJSONMenu.ParserJSONMenu(data);
-                        listCon.get(index).setListCon(subCategories);
-                        notifyDataSetChanged(); // Cập nhật UI khi dữ liệu đã được tải
-                    }
-
-                    @Override
-                    public void onError(String error) {
-                        // Xử lý lỗi nếu cần
-                    }
-                });
-            }
-        }
-
-        @Override
-        public int getGroupCount() {
-            return listCon.size();
-        }
-
-        @Override
-        public int getChildrenCount(int vitriGroupCha) {
-            return listCon.get(vitriGroupCha).getListCon().size();
-        }
-
-        @Override
-        public Object getGroup(int vitriGroupCha) {
-            return listCon.get(vitriGroupCha);
-        }
-
-        @Override
-        public Object getChild(int vitriGroupCha, int vitriGroupCon) {
-            return listCon.get(vitriGroupCha).getListCon().get(vitriGroupCon);
-        }
-
-        @Override
-        public long getGroupId(int vitriGroupCha) {
-            return listCon.get(vitriGroupCha).getMALOAISP();
-        }
-
-        @Override
-        public long getChildId(int vitriGroupCha, int vitriGroupCon) {
-            return listCon.get(vitriGroupCha).getListCon().get(vitriGroupCon).getMALOAISP();
-        }
-
-        @Override
-        public boolean hasStableIds() {
-            return false;
-        }
-
-        @Override
-        public View getGroupView(int vitriGroupCha, boolean isExpanded, View view, ViewGroup viewGroup) {
-            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View viewGroupCha = layoutInflater.inflate(R.layout.custum_lauout_group_cha, viewGroup, false);
-            TextView txtTenLoaiSP = (TextView) viewGroupCha.findViewById(R.id.txtTenLoaiSP);
-            txtTenLoaiSP.setText(listCon.get(vitriGroupCha).getTENLOAISP());
-
-            return viewGroupCha;
-        }
-
-        @Override
-        public View getChildView(int vitriGroupCha, int vitriGroupCon, boolean isLastChild, View view, ViewGroup viewGroup) {
-           TextView tv = new TextView(context);
-           tv.setText(listCon.get(vitriGroupCha).getListCon().get(vitriGroupCon).getTENLOAISP());
-           tv.setPadding(15,5,5,5);
-//           tv.setBackgroundColor(Color.YELLOW);
-           tv.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-
-
-            return tv;
-        }
-
-        @Override
-        public boolean isChildSelectable(int groupPosition, int childPosition) {
-            return false;
-        }
-    }
+//    public class SecondAdapter extends BaseExpandableListAdapter{
+//        List<LoaiSanPham> listCon;
+//        public SecondAdapter(List<LoaiSanPham> listCon){
+//            this.listCon = listCon;
+//            XuLyJSONMenu xuLyJSONMenu = new XuLyJSONMenu();
+//
+//            for (int i = 0; i < listCon.size(); i++) {
+//                int maloaisp = listCon.get(i).getMALOAISP();
+//                final int index = i; // Make the variable effectively final
+//                xuLyJSONMenu.LayLoaiSanPhamThaoMaLoai(maloaisp, new DownloadJSONCallback() {
+//                    @Override
+//                    public void onSuccess(String data) {
+//                        List<LoaiSanPham> subCategories = xuLyJSONMenu.ParserJSONMenu(data);
+//                        listCon.get(index).setListCon(subCategories);
+//                        notifyDataSetChanged(); // Cập nhật UI khi dữ liệu đã được tải
+//                    }
+//
+//                    @Override
+//                    public void onError(String error) {
+//                        // Xử lý lỗi nếu cần
+//                    }
+//                });
+//            }
+//        }
+//
+//        @Override
+//        public int getGroupCount() {
+//            return listCon.size();
+//        }
+//
+//        @Override
+//        public int getChildrenCount(int vitriGroupCha) {
+//            return listCon.get(vitriGroupCha).getListCon().size();
+//        }
+//
+//        @Override
+//        public Object getGroup(int vitriGroupCha) {
+//            return listCon.get(vitriGroupCha);
+//        }
+//
+//        @Override
+//        public Object getChild(int vitriGroupCha, int vitriGroupCon) {
+//            return listCon.get(vitriGroupCha).getListCon().get(vitriGroupCon);
+//        }
+//
+//        @Override
+//        public long getGroupId(int vitriGroupCha) {
+//            return listCon.get(vitriGroupCha).getMALOAISP();
+//        }
+//
+//        @Override
+//        public long getChildId(int vitriGroupCha, int vitriGroupCon) {
+//            return listCon.get(vitriGroupCha).getListCon().get(vitriGroupCon).getMALOAISP();
+//        }
+//
+//        @Override
+//        public boolean hasStableIds() {
+//            return false;
+//        }
+//
+//        @Override
+//        public View getGroupView(int vitriGroupCha, boolean isExpanded, View view, ViewGroup viewGroup) {
+//            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//            View viewGroupCha = layoutInflater.inflate(R.layout.custum_lauout_group_cha, viewGroup, false);
+//            TextView txtTenLoaiSP = (TextView) viewGroupCha.findViewById(R.id.txtTenLoaiSP);
+//            txtTenLoaiSP.setText(listCon.get(vitriGroupCha).getTENLOAISP());
+//
+//            return viewGroupCha;
+//        }
+//
+//        @Override
+//        public View getChildView(int vitriGroupCha, int vitriGroupCon, boolean isLastChild, View view, ViewGroup viewGroup) {
+//           TextView tv = new TextView(context);
+//           tv.setText(listCon.get(vitriGroupCha).getListCon().get(vitriGroupCon).getTENLOAISP());
+//           tv.setPadding(15,5,5,5);
+////           tv.setBackgroundColor(Color.YELLOW);
+//           tv.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+//
+//
+//            return tv;
+//        }
+//
+//        @Override
+//        public boolean isChildSelectable(int groupPosition, int childPosition) {
+//            return false;
+//        }
+//    }
 
 }
