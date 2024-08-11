e4806415a29d6fcd224d1a17b8f43dd0f5588c9f
Nguyễn Tấn Trung <133759843+namaht123@users.noreply.github.com>
2024-08-11 08:29:03 +0700

    form dang ky (p14)
    form dang ky (p14)

--- app/src/main/java/com/example/ung_dung_dat_hang/View/DangNhap/Fragment/FragmentDangKy.java
+++ app/src/main/java/com/example/ung_dung_dat_hang/View/DangNhap/Fragment/FragmentDangKy.java
@@ -9,10 +9,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.ung_dung_dat_hang.R;
+
 public class FragmentDangKy extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.layout_fragment_dangky,container,false);
+        return view;
     }
 }
