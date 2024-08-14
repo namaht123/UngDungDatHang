16436a789556fdbc050252b721c5842cf0fdf2ed
namaht123 <133759843+namaht123@users.noreply.github.com>
2024-08-14 10:12:42 +0700

    show cac trang chi tiet san pham

--- app/src/main/java/com/example/ung_dung_dat_hang/Adapter/SPnhavadoisongAdapter.java
+++ app/src/main/java/com/example/ung_dung_dat_hang/Adapter/SPnhavadoisongAdapter.java
@@ -1,5 +1,6 @@
 package com.example.ung_dung_dat_hang.Adapter;
 
+import android.content.Intent;
 import android.graphics.Paint;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +15,7 @@ import com.bumptech.glide.Glide;
 import com.example.ung_dung_dat_hang.ConnnectInternet.SessionManager;
 import com.example.ung_dung_dat_hang.Model.ObjeactClass.SanPham;
 import com.example.ung_dung_dat_hang.R;
+import com.example.ung_dung_dat_hang.View.ChiTietSanPham.ChiTietSanPhamActivity;
 
 import java.util.List;
 
@@ -79,6 +81,16 @@ public class SPnhavadoisongAdapter extends RecyclerView.Adapter<SPnhavadoisongAd
             holder.giagoc.setPaintFlags(holder.giagoc.getPaintFlags() & (~Paint.STRIKE_THRU_TEXT_FLAG));
             holder.tvGia.setText(""); // Clear the discounted price field
         }
+        holder.itemView.setOnClickListener(v -> {
+            Intent intent = new Intent(holder.itemView.getContext(), ChiTietSanPhamActivity.class);
+            intent.putExtra("MA_SP", sanPham.getMaSP());
+            intent.putExtra("TEN_SP", sanPham.getTenSP());
+            intent.putExtra("GIA_SP", giaGoc);
+            intent.putExtra("THONGTIN_SP", sanPham.getThongTin());
+            intent.putExtra("ANH_SP", sanPham.getAnh());
+            intent.putExtra("ANH_NHO_SP", sanPham.getAnhNho());
+            holder.itemView.getContext().startActivity(intent);
+        });
     }
 
     @Override
